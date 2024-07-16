be888705df6ad7426455549990c58825044140bf
ART4578 <artur17-00@inbox.ru>
2024-07-16 16:40:31 +0400

    Web__Apps__React

--- Fetch__Users/src/App.jsx
+++ Fetch__Users/src/App.jsx
@@ -1,43 +1,61 @@
-import { useEffect, useState } from "react";
-import UsersList from "./Components/UsersList/UsersList";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  //users data
   const [users, setUsers] = useState([]);
   const [staticUsers, setStaticUsers] = useState([]);
-  const [inpValue, setInpValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
+  //fetch users
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then(response => response.json())
+      .then(json => {
+        setUsers(json);
+        setStaticUsers(json);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  //delete user
   const deleteButton = (id) => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
-  useEffect(() => {
-    fetch("https://api.github.com/users")
-      .then((responce) => responce.json())
-      .then((json) => {
-        setUsers(json);
-        setStaticUsers(json);
-      });
-  }, []);
+  //change input value
+  const inputChange = (e) => setInputValue(e.target.value);
 
-  const inputChangeHandler = (e) => {
-    setInpValue(e.target.value);
-  };
-
+  //search users
   useEffect(() => {
-    if (inpValue.length > 2) {
-      setUsers(staticUsers.filter((user) => user.login.includes(inpValue)));
+    if (inputValue.length > 2) {
+      setUsers(staticUsers.filter((user) => user.login.includes(inputValue)));
     } else {
       setUsers(staticUsers);
-    }
-  }, [inpValue]);
+    };
+  }, [inputValue]);
 
   return (
     <div>
-      <input className="input" value={inpValue} onChange={inputChangeHandler} placeholder="Enter the person's name"/>
-      <UsersList users={users} deleteButton={deleteButton} />
+      {/*input*/}
+      <input className="input" value={inputValue} onChange={inputChange} placeholder="Enter Your Name"/>
+      {/*users*/}
+      <div className="users-container">
+      {
+        users.map((user) => {
+          return (
+            <div className="user-container" key={user.id}>
+              <button className="btn" onClick={() => deleteButton(user.id)}>X</button>
+              <img src={user.avatar_url} alt="image" className="img" />
+              <span>ID: {user.id}</span>
+              <h3>Name: {user.login}</h3>
+            </div>
+          );
+        })
+      }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
